b795f65358aa172c940d877ed564292388d87f72
dvlanders <dlanders0705@gmail.com>
2024-05-24 15:36:17 -0400

    add user auth

--- src/routes/utils.js
+++ src/routes/utils.js
@@ -1,5 +1,6 @@
 const { utils } = require("../controllers");
+const { authorizeUser } = require("../util/middleware");
 
 module.exports = (router) => {
-  router.get("/utils/exchangeRate", utils.exchangeRate);
+  router.get("/utils/exchangeRate", authorizeUser, utils.exchangeRate);
 };
