f38e2f6d4d37e7f8c7fdb157f1c0194dd612a81a
samuyoon <84159092+samuyoon@users.noreply.github.com>
2024-05-28 16:24:24 -0400

    fix: adding bridge v account id to req body

--- src/routes/checkbook.js
+++ src/routes/checkbook.js
@@ -7,5 +7,7 @@ module.exports = (router) => {
 	router.post("/checkbook/create_user", authorizeUser, checkbook.createCheckbookUser);
 	router.post("/checkbook/account/plaid", checkbook.createCheckbookBankAccountWithProcessorToken);
 	router.post("/checkbook/account/bridge_virtual_account", checkbook.createCheckbookAccountForBridgeVirtualAccount);
+	router.post("/checkbook/execute_pull_transaction", checkbook.executeCheckbookPullTransaction);
+
 
 };
