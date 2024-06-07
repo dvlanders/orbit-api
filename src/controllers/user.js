120753d559b1db3cc530f47803e607ab8e69d018
samuyoon <84159092+samuyoon@users.noreply.github.com>
2024-06-07 10:15:13 -0400

    formatting

---
+++ src/controllers/user.js
@@ -0,0 +1,53 @@
+const { logger } = require('../util/logger/logger');
+const fetch = require('node-fetch');
+const supabase = require('../util/supabaseClient');
+const { v4: uuidv4 } = require("uuid");
+const { request } = require('express');
+const fundMaticPolygon = require('../util/bastion/fundMaticPolygon');
+const { bastion } = require('.');
+
+
+exports.getPing = async (req, res) => {
+	if (req.method !== 'GET') {
+		return res.status(405).json({ error: 'Method not allowed' });
+	}
+	return res.status(200).json({ message: 'pong' });
+};
+
+exports.createHifiUser = async (req, res) => {
+	if (req.method !== 'POST') {
+		return res.status(405).json({ error: 'Method not allowed' });
+	}
+
+
+
+	const { userId } = req.body;
+
+
+	if (!userId) {
+		return res.status(400).json({ error: 'userId is required' });
+	}
+
+	// create bastion user (comes with wallets)
+
+	// create checkbook user
+
+	// create bridge Customer via customers api
+
+	// create bridge 
+
+	try {
+		const data = await createUserCore(merchantId);
+		res.status(201).json(data);
+	} catch (error) {
+		logger.error(`Something went wrong while creating user: ${error.toString()}`);
+		const { data: logData, error: logError } = await supabase
+			.from('logs')
+			.insert({
+				log: error.toString(),
+				merchant_id: merchantId,
+				endpoint: '/bastion/createUser'
+			})
+		res.status(500).json({ error: `Something went wrong: ${error}` });
+	}
+};
