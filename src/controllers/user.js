b6a50085cfb41d3f978d2ec038fb086033801d10
samuyoon <84159092+samuyoon@users.noreply.github.com>
2024-06-10 11:37:00 -0400

    structuring the user controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -36,8 +36,12 @@ exports.createHifiUser = async (req, res) => {
 	/*
 		let invalidFields = []
 	*/
+
+	// Create the Bastion user w/ wallet addresses. Fund the polygon wallet.
 	try {
 		const bastionResult = await createAndFundBastionUser(userId);
+
+		// if the bastionResult returns any error, update the endpoint's response object to reflect
 		if (bastionResult.status !== 201) {
 			console.log('status not 201')
 			createHifiUserResponse.status = bastionResult.status;
@@ -45,6 +49,20 @@ exports.createHifiUser = async (req, res) => {
 			createHifiUserResponse.message = bastionResult.message;
 			createHifiUserResponse.additionalDetails = bastionResult.additionalDetails;
 		}
+
+
+
+
+	} catch (error) {
+		createLog('createHifiUser', userId, 'Failed to create hifi user', JSON.stringify(error));
+
+		return res.status(500).json(createHifiUserResponse);
+	}
+
+
+	// TODO: William
+	// 	Create the Checkbook user
+	try {
 
 		// Assuming similar functions exist for these tasks
 		// await createCheckbookUser(userId);
@@ -60,22 +78,42 @@ exports.createHifiUser = async (req, res) => {
 		// 	}
 		// }
 
-
-		// determine the status code to return to the client
-		if (createHifiUserResponse.status === 200 || createHifiUserResponse.status === 201) {
-			createHifiUserResponse.message = "User created successfully";
-		} else if (createHifiUserResponse.invalidFields.length === 0) {
-			console.log('in 400 block')
-			createHifiUserResponse.status = 400;
-		} else {
-			createHifiUserResponse.status = 500;
-		}
-
-
-		return res.status(createHifiUserResponse.status).json(createHifiUserResponse);
 	} catch (error) {
-		createLog('createHifiUser', userId, 'Failed to create hifi user', JSON.stringify(error));
-
-		return res.status(500).json(createHifiUserResponse);
+		return res.status(500).json({ error: 'Failed to create user' });
 	}
+
+	// TODO: William
+	// Create the Bridge customer
+	try {
+
+		// Assuming similar functions exist for these tasks
+		// await createCheckbookUser(userId);
+		// await createBridgeCustomer(userId);
+
+		// const createBridgeCustomerResult = await createBridgeCustomer(userId);
+		// if (createBridgeCustomerResult.status !== 200) {
+		// 	createHifiUserResponse.onrampStatus = 'FAILED';
+		// 	createHifiUserResponse.status = createBridgeCustomerResult.status;
+		// 	createHifiUserResponse.message = createBridgeCustomerResult.message;
+		// 	if (createBridgeCustomerResult.invalidFields.length > 0) {
+		// 		createHifiUserResponse.invalid_fields = [...createHifiUserResponse.invalid_fields, createBridgeCustomerResult.invalidFields]
+		// 	}
+		// }
+
+	} catch (error) {
+		return res.status(500).json({ error: 'Failed to create user' });
+	}
+
+	// determine the status code to return to the client
+	if (createHifiUserResponse.status === 200 || createHifiUserResponse.status === 201) {
+		createHifiUserResponse.message = "User created successfully";
+	} else if (createHifiUserResponse.invalidFields.length === 0) {
+		console.log('in 400 block')
+		createHifiUserResponse.status = 400;
+	} else {
+		createHifiUserResponse.status = 500;
+	}
+
+
+	return res.status(createHifiUserResponse.status).json(createHifiUserResponse);
 };
