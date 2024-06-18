7916d738c36d7991716c8143311d90bf78125c6a
dvlanders <dlanders0705@gmail.com>
2024-06-18 17:20:02 -0400

    add auth

--- src/routes/account.js
+++ src/routes/account.js
@@ -3,11 +3,11 @@ const { account } = require("../controllers");
 const { authorize } = require("../util/middleware");
 
 module.exports = (router) => {
-	router.post("/account/usd/onramp/plaid", account.createUsdOnrampSourceWithPlaid); // checkbook
-	router.post("/account/usd/offramp", account.createUsdOfframpDestination); // bridge external account
+	router.post("/account/usd/onramp/plaid", authorize, account.createUsdOnrampSourceWithPlaid); // checkbook
+	router.post("/account/usd/offramp", authorize, account.createUsdOfframpDestination); // bridge external account
 
 	// router.post("/account/euro/onramp", account.createBankAccount); // not possible currently
-	router.post("/account/euro/offramp", account.createEuroOfframpDestination);// bridge external account
-	router.get("/account", account.getAccount);// bridge external account
+	router.post("/account/euro/offramp", authorize, account.createEuroOfframpDestination);// bridge external account
+	router.get("/account", authorize, account.getAccount);// bridge external account
 
 };
