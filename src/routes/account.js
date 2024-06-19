e692b96627463b2d3f227f0a83f27e3b0f1e1c32
dvlanders <dlanders0705@gmail.com>
2024-06-19 13:04:05 -0400

    add auth

--- src/routes/account.js
+++ src/routes/account.js
@@ -5,9 +5,8 @@ const { authorize, authorizeSupabase } = require("../util/middleware");
 module.exports = (router) => {
 	router.post("/account/usd/onramp/plaid", authorize, account.createUsdOnrampSourceWithPlaid); // checkbook
 	router.post("/account/usd/offramp", authorize, account.createUsdOfframpDestination); // bridge external account
-	router.post("/account/activateOnRampRail", account.activateOnRampRail)
+	router.post("/account/activateOnRampRail", authorize, account.activateOnRampRail)
 	// router.post("/account/euro/onramp", account.createBankAccount); // not possible currently
 	router.post("/account/euro/offramp", authorize, account.createEuroOfframpDestination);// bridge external account
 	router.get("/account", authorize, account.getAccount);// bridge external account
-
 };
