eb74b1cf0682451cb91da5ec657b717c6f60b5f5
samuyoon <84159092+samuyoon@users.noreply.github.com>
2024-06-27 15:21:46 -0400

    feat: creat biz customer and update

--- src/util/bastion/endpoints/submitKycData.js
+++ src/util/bastion/endpoints/submitKycData.js
@@ -21,44 +21,80 @@ class submitBastionKycError extends Error {
 	}
 }
 
-const submitKycData = async(userId) => {
-    // fetch user info
-    const { data: userKyc, error: userKycError } = await supabaseCall(() => supabase
-    .from("user_kyc")
-    .select('legal_first_name, legal_last_name, ip_address, date_of_birth')
-    .eq("user_id", userId)
-    .maybeSingle()
-    )
+const submitKycData = async (userId) => {
+	// Check user type
+	const { data: userTypeData, error: userTypeError } = await supabaseCall(() => supabase
+		.from("users")
+		.select('user_type')
+		.eq("id", userId)
+		.maybeSingle()
+	);
 
-    if (userKycError) throw new submitBastionKycError(submitBastionKycErrorType.INTERNAL_ERROR, userKycError.message, userKycError)
-    if (!userKyc) throw new submitBastionKycError(submitBastionKycErrorType.RECORD_NOT_FOUND, "user not found")
+	if (userTypeError) throw new submitBastionKycError(submitBastionKycErrorType.INTERNAL_ERROR, userTypeError.message, userTypeError);
+	if (!userTypeData) throw new submitBastionKycError(submitBastionKycErrorType.RECORD_NOT_FOUND, "User not found");
 
-    const birthDate = userKyc.date_of_birth ? new Date(userKyc.date_of_birth) : undefined;
-    const formattedBirthDate = `${birthDate.getUTCFullYear()}-${(birthDate.getUTCMonth() + 1).toString().padStart(2, '0')}-${birthDate.getUTCDate().toString().padStart(2, '0')}`;
+	// Get user kyc data regardless of type bc we need the ip_address at minimum
+	const { data: userKycData, error: userKycError } = await supabaseCall(() => supabase
+		.from("user_kyc")
+		.select('legal_first_name, legal_last_name, date_of_birth, ip_address')
+		.eq("user_id", userId)
+		.maybeSingle()
+	);
 
-    const requestBody = {
-        firstName: userKyc.legal_first_name,
-        lastName: userKyc.legal_last_name,
-        dateOfBirth: formattedBirthDate,
-        ipAddress: userKyc.ip_address
-    };
 
-    const url = `${BASTION_URL}/v1/users/${userId}/kyc`;
-    const options = {
-        method: 'POST',
-        headers: {
-            accept: 'application/json',
-            'content-type': 'application/json',
-            Authorization: `Bearer ${BASTION_API_KEY}`
-        },
-        body: JSON.stringify(requestBody)
-    };
 
-    const response = await fetch(url, options);
-    return response
+	if (userKycError) throw new submitBastionKycError(submitBastionKycErrorType.INTERNAL_ERROR, userKycError.message, userKycError);
+	if (!userKycData) throw new submitBastionKycError(submitBastionKycErrorType.RECORD_NOT_FOUND, "User kyc record not found");
+
+	let requestBody = {
+		firstName: userKycData.legal_first_name,
+		lastName: userKycData.legal_last_name,
+		dateOfBirth: userKycData.date_of_birth ? formatDate(userKycData.date_of_birth) : undefined,
+		ipAddress: userKycData.ip_address
+	};
+
+	// If business user, replace the name and date of birth with the primary UBO's details
+	if (userTypeData.user_type === 'business') {
+		const { data: primaryUbo, error: uboError } = await supabaseCall(() => supabase
+			.from("ultimate_beneficial_owners")
+			.select('*')
+			.eq("user_id", userId)
+			.eq("is_primary", true)
+			.maybeSingle()
+		);
+
+		if (uboError) throw new submitBastionKycError(submitBastionKycErrorType.INTERNAL_ERROR, uboError.message, uboError);
+		if (!primaryUbo) throw new submitBastionKycError(submitBastionKycErrorType.RECORD_NOT_FOUND, "Primary UBO not found");
+
+		requestBody.firstName = primaryUbo.legal_first_name;
+		requestBody.lastName = primaryUbo.legal_last_name;
+		requestBody.dateOfBirth = primaryUbo.date_of_birth ? formatDate(primaryUbo.date_of_birth) : undefined;
+	}
+
+
+	// Perform the KYC submission
+	const url = `${BASTION_URL}/v1/users/${userId}/kyc`;
+	const options = {
+		method: 'POST',
+		headers: {
+			accept: 'application/json',
+			'content-type': 'application/json',
+			Authorization: `Bearer ${BASTION_API_KEY}`
+		},
+		body: JSON.stringify(requestBody)
+	};
+
+	const response = await fetch(url, options);
+	return response;
+};
+
+// Helper function to format the date
+function formatDate(dateString) {
+	const birthDate = new Date(dateString);
+	return `${birthDate.getUTCFullYear()}-${(birthDate.getUTCMonth() + 1).toString().padStart(2, '0')}-${birthDate.getUTCDate().toString().padStart(2, '0')}`;
 }
 
 
 module.exports = {
-    submitKycData
+	submitKycData
 }
---
+++ src/util/bridge/endpoint/updateBusinessBridgeCustomer.js
@@ -0,0 +1,113 @@
+const supabase = require("../../supabaseClient");
+const { v4 } = require("uuid");
+const fileToBase64 = require("../../fileToBase64");
+const createLog = require("../../logger/supabaseLogger");
+const { supabaseCall } = require("../../supabaseWithRetry")
+const { BridgeCustomerStatus } = require("../utils")
+const { createBusinessBridgeCustomer } = require("./submitBusinessBridgeCustomerApplication")
+
+
+const UpdateBridgeCustomerErrorType = {
+	RECORD_NOT_FOUND: "RECORD_NOT_FOUND",
+	INVALID_FIELD: "INVALID_FIELD",
+	INTERNAL_ERROR: "INTERNAL_ERROR",
+};
+
+class UpdateBridgeCustomerError extends Error {
+	constructor(type, message, rawResponse) {
+		super(message);
+		this.type = type;
+		this.rawResponse = rawResponse;
+		Object.setPrototypeOf(this, UpdateBridgeCustomerError.prototype);
+	}
+}
+
+exports.updateBusinessBridgeCustomer = async (userId) => {
+	try {
+		// check user bridge kyc status 
+		let { data: bridgeCustomer, error: bridgeCustomerError } = await supabaseCall(() => supabase
+			.from('bridge_customers')
+			.select('*')
+			.eq("user_id", userId)
+			.maybeSingle()
+		)
+
+		if (bridgeCustomerError) throw new UpdateBridgeCustomerError(UpdateBridgeCustomerErrorType.INTERNAL_ERROR, bridgeCustomerError.message, bridgeCustomerError)
+		if (!bridgeCustomer) throw new UpdateBridgeCustomerError(UpdateBridgeCustomerErrorType.INTERNAL_ERROR, "No user record found")
+		if (!bridgeCustomer.status || !bridgeCustomer.bridge_id) {
+			// resubmit application
+			return await createBusinessBridgeCustomer(userId)
+		}
+
+		// update application
+		return await createBusinessBridgeCustomer(userId, bridgeCustomer.bridge_id, true)
+
+
+
+	} catch (error) {
+		//  log
+		createLog("user/util/updateBusinessBridgeCustomer", userId, error.message, error.rawResponse)
+		// process error
+		if (error.type == UpdateBridgeCustomerErrorType.INTERNAL_ERROR) {
+			return {
+				status: 500,
+				customerStatus: {
+					status: CustomerStatus.INACTIVE,
+					actions: [],
+					fields: []
+				},
+				usRamp: {
+					status: CustomerStatus.INACTIVE,
+					actions: [],
+					fields: []
+				},
+				euRamp: {
+					status: CustomerStatus.INACTIVE,
+					actions: [],
+					fields: []
+				},
+				message: "Please contact HIFI for more information"
+			}
+		} else if (error.type == UpdateBridgeCustomerErrorType.RECORD_NOT_FOUND) {
+			return {
+				status: 500,
+				customerStatus: {
+					status: CustomerStatus.INACTIVE,
+					actions: [],
+					fields: []
+				},
+				usRamp: {
+					status: CustomerStatus.INACTIVE,
+					actions: [],
+					fields: []
+				},
+				euRamp: {
+					status: CustomerStatus.INACTIVE,
+					actions: [],
+					fields: []
+				},
+				message: "Please contact HIFI for more information"
+			}
+		}
+
+		return {
+			status: 500,
+			customerStatus: {
+				status: CustomerStatus.INACTIVE,
+				actions: [],
+				fields: []
+			},
+			usRamp: {
+				status: CustomerStatus.INACTIVE,
+				actions: [],
+				fields: []
+			},
+			euRamp: {
+				status: CustomerStatus.INACTIVE,
+				actions: [],
+				fields: []
+			},
+			message: "Please contact HIFI for more information"
+		}
+	}
+}
