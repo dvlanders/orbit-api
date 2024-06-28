98293a41b4f830728ac1565b87f19be8906b8acc
samuyoon <84159092+samuyoon@users.noreply.github.com>
2024-06-28 13:30:01 -0400

    fix: checkbook account id to accountId

--- src/util/account/getAccount/main/fetchPlaidAccountInformation.js
+++ src/util/account/getAccount/main/fetchPlaidAccountInformation.js
@@ -2,27 +2,27 @@ const supabase = require("../../../supabaseClient")
 const { supabaseCall } = require("../../../supabaseWithRetry")
 
 
-const fetchPlaidAccountInformation = async(accountId) => {
-    let { data: checkbookAccount, error } = await supabaseCall(() => supabase
-    .from('checkbook_accounts')
-    .select('id, created_at, account_type, account_number, routing_number, bank_name')
-    .eq("id", accountId)
-    .eq("connected_account_type", "PLAID")
-    .maybeSingle())
+const fetchPlaidAccountInformation = async (accountId) => {
+	let { data: checkbookAccount, error } = await supabaseCall(() => supabase
+		.from('checkbook_accounts')
+		.select('id, created_at, account_type, account_number, routing_number, bank_name')
+		.eq("id", accountId)
+		.eq("connected_account_type", "PLAID")
+		.maybeSingle())
 
-    if (error) throw error
-    if (!checkbookAccount) return null
+	if (error) throw error
+	if (!checkbookAccount) return null
 
-    const bankInfo = {
-        id: checkbookAccount.id,
-        createdAt: checkbookAccount.createdAt,
-        accountType: checkbookAccount.account_type,
-        accountNumber: checkbookAccount.account_number,
-        routingNumber: checkbookAccount.routing_number,
-        bankName: checkbookAccount.bank_name,
-    }
+	const bankInfo = {
+		accountId: checkbookAccount.id,
+		createdAt: checkbookAccount.createdAt,
+		accountType: checkbookAccount.account_type,
+		accountNumber: checkbookAccount.account_number,
+		routingNumber: checkbookAccount.routing_number,
+		bankName: checkbookAccount.bank_name,
+	}
 
-    return bankInfo
+	return bankInfo
 }
 
 module.exports = fetchPlaidAccountInformation
