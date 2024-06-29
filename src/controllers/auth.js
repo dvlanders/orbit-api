d6661650ac8bc53c8121682ec2e622d6e16da87b
dvlanders <dlanders0705@gmail.com>
2024-06-29 18:54:17 -0400

    add activate webhook endpoint for dashboard

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,6 +4,7 @@ const { fieldsValidation } = require("../util/common/fieldsValidation");
 const { verifyToken } = require("../util/helper/verifyToken");
 const createLog = require("../util/logger/supabaseLogger");
 const { supabaseCall } = require("../util/supabaseWithRetry");
+const activateWebhook = require("../util/auth/webhook/createWebhookUrl");
 
 exports.createApiKey = async(req, res) => {
     if (req.method !== 'POST') {
@@ -83,5 +84,63 @@ exports.createProfileInSandbox = async(req, res) => {
     }catch (error){
         createLog("auth/createProfileInSandbox", "", `Fail to create profile for: ${record.id}, ${error.message}`)
         return res.status(500).json({error: "Unexpected error happened"})
+    }
+}
+
+exports.createWebhook = async(req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method not allowed' });
+    }
+
+    try{
+        // this token will be supabase auth token
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            return res.status(401).json({error: "Not authorized"});
+        };
+
+        // get user info (user_id)
+        const user = await verifyToken(token);
+        if (!user && !user?.sub) {
+            return res.status(401).json({ error: "Not authorized" });
+        };
+
+        const profileId = user.sub
+        const { webhookUrl, env } = req.body
+        // filed validation
+        if (!webhookUrl || !env) return res.status(400).json({error: "webhookUrl and env is required"})
+
+        // dev spin up sandbox profile if not yet exist
+        if (env == "sandbox"){
+            const supabase = createClient(process.env.SUPABASE_SANDBOX_URL, process.env.SUPABASE_SANDBOX_SERVICE_ROLE_KEY)
+            const {data: sandboxProfile, error: sandboxProfileError} = await supabaseCall(() => supabase
+                .from("profiles")
+                .select("id")
+                .eq("id", profileId)
+                .maybeSingle())
+            
+            if (sandboxProfileError) throw sandboxProfileError
+            if (!sandboxProfile){
+                // insert new profile
+                const {data: newSandboxProfile, error: newSandboxProfileError} = await supabaseCall(() => supabase
+                    .from("profiles")
+                    .insert({
+                        id: profileId
+                    }))
+                if (newSandboxProfileError) throw newSandboxProfileError
+            }
+        }
+
+        const secretKey = await activateWebhook(webhookUrl, profileId, env)
+        const result = {
+            webhookUrl,
+            secretKey
+        }
+        return res.status(200).json(result)
+
+    }catch (error){
+        console.error(error)
+        createLog("auth/createWebhookUrl", "", error.message, error)
+        return res.status(500).json({error: "Internal server error"})
     }
 }
