91cd4a8cbc671abd35ed92605766857ea1601f60
dvlanders <dlanders0705@gmail.com>
2024-07-01 17:24:51 -0400

    add webhook job clean every 24hrs

--- cron/index.js
+++ cron/index.js
@@ -5,6 +5,7 @@ const pollBridgeCustomerStatus = require('./jobs/pollBridgeCustomerStatus');
 const pollOnrampTransactionsCheckbookStatus = require('./jobs/pollOnrampTransactionsCheckbookStatus');
 const pollBastionCryptoToCryptoTransferStatus = require('./jobs/pollBastionCryptoToCryptoTransferStatus');
 const pollWebhookRetry = require('./jobs/pollWebhookRetry');
+const pollCleanWebhookQueue = require('./jobs/utils/pollCleanWebhookQueue');
 
 cron.schedule('*/60 * * * * *', pollOfframpTransactionsBridgeStatus); // every 60 seconds
 cron.schedule('*/60 * * * * *', pollOfframpTransactionsBastionStatus); // every 60 seconds
@@ -12,5 +13,6 @@ cron.schedule('*/60 * * * * *', pollBridgeCustomerStatus); // every 60 seconds
 cron.schedule('*/60 * * * * *', pollOnrampTransactionsCheckbookStatus); // every 60 seconds
 cron.schedule('*/60 * * * * *', pollBastionCryptoToCryptoTransferStatus); // every 60 seconds
 cron.schedule('*/20 * * * * *', pollWebhookRetry);
+cron.schedule('0 0 * * *', pollCleanWebhookQueue); // every 24 hrs
 
 
