f1775db066a722ec3166db0e6390696a2a2e5579
dvlanders <dlanders0705@gmail.com>
2024-07-15 13:11:16 -0400

    remove hard coded value

--- src/util/bridge/endpoint/fetchBridgeVirtualAccountMicroDeposit.js
+++ src/util/bridge/endpoint/fetchBridgeVirtualAccountMicroDeposit.js
@@ -7,9 +7,7 @@ const BRIDGE_URL = process.env.BRIDGE_URL;
 const fetchBridgeVirtualAccountMicroDeposit = async(userId, virtualAccountId, limit=10, createdBefore , createdAfter) => {
     try{
 
-        // const bridgeId = await getBridgeUserId(userId)
-        const bridgeId = "a8245c09-12af-4f91-8ec5-e95e5be81717"
-        const virtualAccountId = "45105ad0-73ab-4164-988a-69c070af4832"
+        const bridgeId = await getBridgeUserId(userId)
         const url = `${BRIDGE_URL}/v0/customers/${bridgeId}/virtual_accounts/${virtualAccountId}/history?event_type=microdeposit&limit=${limit}${createdBefore? `&starting_after=${createdBefore}`: ""}${createdAfter? `&ending_before=${createdAfter}`: ""}`
         const response = await fetch(url, {
             method: 'GET',
@@ -22,7 +20,7 @@ const fetchBridgeVirtualAccountMicroDeposit = async(userId, virtualAccountId, li
 
         if (!response.ok) {
             console.error(responseBody)
-            // createLog("bridge/fetchBridgeVirtualAccountMicroDeposit", userId, "Fail to fetch Bridge Virtual Account Micro Deposit", responseBody)
+            createLog("bridge/fetchBridgeVirtualAccountMicroDeposit", userId, "Fail to fetch Bridge Virtual Account Micro Deposit", responseBody)
             return {count: 0, data: [], message: "something went wrong, please contact HIFI for more information"}
         }
 
@@ -47,7 +45,7 @@ const fetchBridgeVirtualAccountMicroDeposit = async(userId, virtualAccountId, li
 
     }catch (error){
         console.error(error)
-        // createLog("bridge/fetchBridgeVirtualAccountMicroDeposit", userId, error.message)
+        createLog("bridge/fetchBridgeVirtualAccountMicroDeposit", userId, error.message)
         return {count: 0, data: [], message: "something went wrong, please contact HIFI for more information"}
     }
 
