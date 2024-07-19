ab4fc24154e4413370096148a755abce370b73d8
samuyoon <84159092+samuyoon@users.noreply.github.com>
2024-07-19 15:13:37 -0400

    fix: inserApiKeyRecord function uses sandbox supabase client util

--- src/util/auth/createApiKey/insertApiKeyRecord.js
+++ src/util/auth/createApiKey/insertApiKeyRecord.js
@@ -1,26 +1,24 @@
-const { createClient } = require("@supabase/supabase-js");
-let supabase = require("../../supabaseClient");
+const supabase = require("../../supabaseClient");
+const supabaseSandbox = require("../util/sandboxSupabaseClient");
 const { supabaseCall } = require("../../supabaseWithRetry");
 
-exports.insertApiKeyRecord = async(info, env) => {
-    if (env == "sandbox") {
-        supabase = createClient(process.env.SUPABASE_SANDBOX_URL, process.env.SUPABASE_SANDBOX_SERVICE_ROLE_KEY)
-    }
+exports.insertApiKeyRecord = async (info, env) => {
+	const client = env === "sandbox" ? supabaseSandbox : supabase;
 
-    const { data: apiKeysDetails, error: apiKeysDetailsError } = await supabaseCall(() => supabase
-        .from('api_keys')
-        .insert({
-            id: info.id,
-            expired_at: info.expiredAt,
-            user_custom_name: info.userCustomName,
-            profile_id: info.profileId,
-            zuplo_api_key_id: info.zuploApiKeyId,
-            zuplo_customer_id: info.zuploCustomerId,
-        })
-        .select("*")
-        .single()
-    )
+	const { data: apiKeysDetails, error: apiKeysDetailsError } = await supabaseCall(() => client
+		.from('api_keys')
+		.insert({
+			id: info.id,
+			expired_at: info.expiredAt,
+			user_custom_name: info.userCustomName,
+			profile_id: info.profileId,
+			zuplo_api_key_id: info.zuploApiKeyId,
+			zuplo_customer_id: info.zuploCustomerId,
+		})
+		.select("*")
+		.single()
+	);
 
-    if (apiKeysDetailsError) throw apiKeysDetailsError
-    return apiKeysDetails
+	if (apiKeysDetailsError) throw apiKeysDetailsError;
+	return apiKeysDetails;
 }
