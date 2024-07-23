7a7d94bb9728809441cc162f9c26cd1005f1a658
Hsin-Hung Wu <hsinhungw@gmail.com>
2024-07-23 16:06:08 -0400

    updates to the virtual account creation

--- test/api/prod/account/activateOnRampRail.test.js
+++ test/api/prod/account/activateOnRampRail.test.js
@@ -3,6 +3,8 @@ const app = require("@/app");
 const { authTestParams, userInfo } = require("../testConfig");
 const { statusChecker } = require("@/test/testUtils");
 
+const USER_ID = "9708a575-5ff9-4008-8ff3-93d1b145d46e"
+
 const activateOnRampRail = async (
   rail,
   destinationCurrency,
@@ -10,7 +12,7 @@ const activateOnRampRail = async (
 ) => {
   const accountRes = await supertest(app)
     .post(
-      `/account/activateOnRampRail?apiKeyId=${authTestParams.API_KEY}&userId=${userInfo.USER_ID}`
+      `/account/activateOnRampRail?apiKeyId=${authTestParams.API_KEY}&userId=${USER_ID}`
     )
     .set({
       "zuplo-secret": authTestParams.ZUPLO_SECRET,
@@ -28,17 +30,29 @@ const activateOnRampRail = async (
   console.log(
     `Account activated on ${rail} rail for ${destinationCurrency} on ${destinationChain}`
   );
-  expect(account.message).toBeDefined();
-  expect(account.message).toMatch(/(successfully|activated)$/);
+
+    if (account.message.includes("successfully")) {
+      expect(account.message).toBe(`${rail} create successfully`)
+      expect(account.account).toBeDefined();
+      const vAccount = account.account;
+      expect(vAccount.virtualAccountId).toBeDefined();
+      expect(vAccount.userId).toBe(USER_ID);
+      expect(vAccount.destinationCurrency).toBe(destinationCurrency)
+      expect(vAccount.destinationChain).toBe(destinationChain)
+      expect(vAccount.railStatus).toBe("activated")
+    } else if (account.message.includes("activated")) {
+      expect(account.message).toBe("rail already activated");
+    }
+
 };
 
 describe("POST /account/activateOnRampRail", function () {
   it("it should has status code 200", async () => {
-    await activateOnRampRail("US_ACH", "usdc", "POLYGON_MAINNET");
-    await activateOnRampRail("US_ACH", "usdc", "ETHEREUM_MAINNET");
-    await activateOnRampRail("US_ACH", "usdt", "ETHEREUM_MAINNET");
-    await activateOnRampRail("US_ACH", "usdc", "OPTIMISM_MAINNET");
+    await activateOnRampRail("US_ACH_WIRE", "usdc", "POLYGON_MAINNET");
+    await activateOnRampRail("US_ACH_WIRE", "usdc", "ETHEREUM_MAINNET");
+    await activateOnRampRail("US_ACH_WIRE", "usdt", "ETHEREUM_MAINNET");
+    await activateOnRampRail("US_ACH_WIRE", "usdc", "OPTIMISM_MAINNET");
     // TODO: is base mainnet supported?
-    // await activateOnRampRail("US_ACH", "usdc", "BASE_MAINNET");
+    // await activateOnRampRail("US_ACH_WIRE", "usdc", "BASE_MAINNET");
   }, 30000);
 });
