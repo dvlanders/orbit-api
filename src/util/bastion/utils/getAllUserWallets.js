4db3f5b5aef6bda9ef57d89d3e63ab5e3f176568
Hsin-Hung Wu <hsinhungw@gmail.com>
2024-07-29 18:08:25 -0400

    Mod get wallet logic

--- src/util/bastion/utils/getAllUserWallets.js
+++ src/util/bastion/utils/getAllUserWallets.js
@@ -1,22 +1,23 @@
 const supabase = require("../../supabaseClient");
 const { supabaseCall } = require("../../supabaseWithRetry");
 
+const prodChains = ["POLYGON_MAINNET"]
+const sandboxChains = ["POLYGON_AMOY"]
+
 exports.getAllUserWallets = async(userId) => {
     
     let { data: bastion_wallets, error } = await supabase
         .from('bastion_wallets')
         .select('chain, address')
         .eq("user_id", userId)
+        .in('chain', process.env.NODE_ENV === 'production' ? prodChains : sandboxChains);
     
     if (error) throw error
 
     const wallets = {}
     bastion_wallets.map((wallet) => {
-        if((process.env.NODE_ENV == "production" && wallet.chain == "POLYGON_MAINNET") ||
-           (process.env.NODE_ENV == "development" && wallet.chain == "POLYGON_AMOY")){
-            wallets[wallet.chain] = {
-                address: wallet.address
-            }
+        wallets[wallet.chain] = {
+            address: wallet.address
         }
     })
 
