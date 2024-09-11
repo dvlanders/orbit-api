b59e2a2a1544f400403eccd8d1efc47d690ddfe5
Hsin-Hung Wu <hsinhungw@gmail.com>
2024-09-11 18:06:05 -0400

    Add missing logging to dashboard endpoints

--- src/routes/dashboard/transfer.js
+++ src/routes/dashboard/transfer.js
@@ -1,16 +1,16 @@
 const { transfer } = require("../../controllers");
-const { authorize, authorizeDashboard, requiredProdDashboard, requiredAdmin } = require("../../util/middleware");
+const { authorize, authorizeDashboard, requiredProdDashboard, requiredAdmin, logRequestResponse } = require("../../util/middleware");
 
 module.exports = (router) => {
-	router.post("/dashboard/transfer/crypto-to-crypto", authorizeDashboard, requiredProdDashboard, transfer.createCryptoToCryptoTransfer);
-	router.post("/dashboard/transfer/crypto-to-crypto/developer", authorizeDashboard, requiredProdDashboard, requiredAdmin, transfer.createCryptoToCryptoTransfer);
-	router.get("/dashboard/transfer/crypto-to-crypto", authorizeDashboard, requiredProdDashboard,transfer.getCryptoToCryptoTransfer)
-	router.get("/dashboard/transfer/crypto-to-crypto/all", authorizeDashboard, requiredProdDashboard,transfer.getAllCryptoToCryptoTransfer)
-	router.post("/dashboard/transfer/crypto-to-fiat", authorizeDashboard, requiredProdDashboard,transfer.createCryptoToFiatTransfer);
-	router.post("/dashboard/transfer/crypto-to-fiat/developer", authorizeDashboard, requiredProdDashboard, requiredAdmin, transfer.createCryptoToFiatTransfer);
-	router.get("/dashboard/transfer/crypto-to-fiat", authorizeDashboard, requiredProdDashboard,transfer.getCryptoToFiatTransfer);
-	router.get("/dashboard/transfer/crypto-to-fiat/all", authorizeDashboard, requiredProdDashboard,transfer.getAllCryptoToFiatTransfer);
-	router.post("/dashboard/transfer/fiat-to-crypto", authorizeDashboard, requiredProdDashboard,transfer.createFiatToCryptoTransfer)
-	router.get("/dashboard/transfer/fiat-to-crypto", authorizeDashboard, requiredProdDashboard,transfer.getFiatToCryptoTransfer)
-	router.get("/dashboard/transfer/fiat-to-crypto/all", authorizeDashboard, requiredProdDashboard,transfer.getAllFiatToCryptoTransfer)
+	router.post("/dashboard/transfer/crypto-to-crypto", authorizeDashboard, requiredProdDashboard, logRequestResponse, transfer.createCryptoToCryptoTransfer);
+	router.post("/dashboard/transfer/crypto-to-crypto/developer", authorizeDashboard, requiredProdDashboard, requiredAdmin, logRequestResponse, transfer.createCryptoToCryptoTransfer);
+	router.get("/dashboard/transfer/crypto-to-crypto", authorizeDashboard, requiredProdDashboard, logRequestResponse, transfer.getCryptoToCryptoTransfer)
+	router.get("/dashboard/transfer/crypto-to-crypto/all", authorizeDashboard, requiredProdDashboard, logRequestResponse, transfer.getAllCryptoToCryptoTransfer)
+	router.post("/dashboard/transfer/crypto-to-fiat", authorizeDashboard, requiredProdDashboard, logRequestResponse, transfer.createCryptoToFiatTransfer);
+	router.post("/dashboard/transfer/crypto-to-fiat/developer", authorizeDashboard, requiredProdDashboard, requiredAdmin, logRequestResponse, transfer.createCryptoToFiatTransfer);
+	router.get("/dashboard/transfer/crypto-to-fiat", authorizeDashboard, requiredProdDashboard, logRequestResponse, transfer.getCryptoToFiatTransfer);
+	router.get("/dashboard/transfer/crypto-to-fiat/all", authorizeDashboard, requiredProdDashboard, logRequestResponse, transfer.getAllCryptoToFiatTransfer);
+	router.post("/dashboard/transfer/fiat-to-crypto", authorizeDashboard, requiredProdDashboard, logRequestResponse, transfer.createFiatToCryptoTransfer)
+	router.get("/dashboard/transfer/fiat-to-crypto", authorizeDashboard, requiredProdDashboard, logRequestResponse, transfer.getFiatToCryptoTransfer)
+	router.get("/dashboard/transfer/fiat-to-crypto/all", authorizeDashboard, requiredProdDashboard, logRequestResponse, transfer.getAllFiatToCryptoTransfer)
 };
