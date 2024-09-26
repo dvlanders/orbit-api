3de59025cd46f4924e002dd36e6876a2fee0e326
dvlanders <dlanders0705@gmail.com>
2024-09-25 21:52:35 -0400

    add middleware

--- src/routes/dashboard/developer.js
+++ src/routes/dashboard/developer.js
@@ -1,7 +1,7 @@
 const { developerDashboard } = require("../../controllers");
-const { logRequestResponse, requiredAdmin, authorizeDashboard } = require("../../util/middleware");
+const { logRequestResponse, requiredAdmin, authorizeDashboard, requiredProdDashboard } = require("../../util/middleware");
 
 
 module.exports = (router) => {
-    router.post("/dashboard/developer/withdraw-gas", authorizeDashboard, requiredAdmin, logRequestResponse, developerDashboard.withdrawFromGasWallet);
+    router.post("/dashboard/developer/withdraw-gas", authorizeDashboard, requiredProdDashboard, requiredAdmin, logRequestResponse, developerDashboard.withdrawFromGasWallet);
 }
