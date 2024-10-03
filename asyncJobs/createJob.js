5dd983c79ce5ee1469e3bd668670227987320978
lakefishingman522 <calvingill89@gmail.com>
2024-10-03 16:25:50 +0000

    add async transfer existing error in OfframpTransaction

--- asyncJobs/createJob.js
+++ asyncJobs/createJob.js
@@ -20,8 +20,7 @@ const createJob = async(job, config, userId, profileId, createdAt=new Date().toI
                 number_of_retries: numberOfRetries + 1,
                 retry_deadline: retryDeadline,
                 retry_interval: retryInterval,
-                env: JOB_ENV,
-                in_process: true
+                env: JOB_ENV
             })
         
         if (error) throw error
