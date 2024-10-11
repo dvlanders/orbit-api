0f115a5b637c12de6f95c09f3ac5fa5a229c4401
lakefishingman522 <calvingill89@gmail.com>
2024-10-11 18:41:53 +0000

    create updateStatus for YC

--- cron/jobs/pollOfframpTransactionsYellowcardStatus.js
+++ cron/jobs/pollOfframpTransactionsYellowcardStatus.js
@@ -1,14 +1,70 @@
-
+const supabase = require('../../src/util/supabaseClient');
+const createLog = require('../../src/util/logger/supabaseLogger');
+const notifyCryptoToFiatTransfer = require('../../webhooks/transfer/notifyCryptoToFiatTransfer');
+const { getBearerDid } = require('../../src/util/yellowcard/utils/getBearerDid');
 
 const updateStatusWithYellowcardTransferId = async (transaction) => {
+    const { data: yellowcardTransactionRecord, error: yellowcardTransactionError } = await supabase
+		.from('yellowcard_transactions')
+		.select('*')
+		.eq('id', offrampTransactionRecord.yellowcard_transaction_id)
+		.maybeSingle();
 
+    if (yellowcardTransactionError || !yellowcardTransactionRecord) {
+        console.error('Error fetching yellowcard transaction record:', yellowcardTransactionError);
+        return { error: "An unexpected error occurred fetching transaction record." };
+    }
+
+    const bearerDid = await getBearerDid();
+
+    // fetch exchange
+    const { TbdexHttpClient, Close } = await import('@tbdex/http-client');
+    const exchange = await TbdexHttpClient.getExchange({
+        pfiDid: yellowcardTransactionRecord.yellowcard_rfq_response.metadata.from,
+        did: bearerDid,
+        exchangeId: yellowcardTransactionRecord.yellowcard_rfq_response.metadata.exchangeId
+    });
+
+    let close;
+    for (const message of exchange) {
+        if (message instanceof Close) {
+            close = message;
+            break;
+        }
+    }
+
+    if (!close) return;
+
+    const hifiOfframpTransactionStatus = close.data.success ? "COMPLETED" : "FAILED_FIAT_RETURNED";
+
+    const { data: updateData, error: updateError } = await supabase
+        .from('offramp_transactions')
+        .update({
+            transaction_status: hifiOfframpTransactionStatus,
+            bridge_transaction_status: data.state,
+            bridge_response: data,
+            updated_at: new Date().toISOString(),
+            failed_reason: close.data.success ? undefined : close.data.reason,
+        })
+        .eq('id', transaction.id)
+        .select()
+        .single()
+    
+    if (updateError) {
+        console.error('Failed to update transaction status', updateError);
+        await createLog('pollOfframpTransactionsBridgeStatus/updateStatusWithBridgeTransferId', transaction.user_id, 'Failed to update transaction status', updateError);
+        return
+    }
+
+    // send webhook message
+    await notifyCryptoToFiatTransfer(updateData);
 }
 
 async function pollOfframpTransactionsYellowcardStatus() {
     const { data: offrampTransactionData, error: offrampTransactionError } = await supabase
 		.from('offramp_transactions')
 		.update({ updated_at: new Date().toISOString() })
-		.eq("fiat_provider", "YLLOWCARD")
+		.eq("fiat_provider", "YELLOWCARD")
 		.eq("transaction_status", "COMPLETED_ONCHAIN")
 		.order('updated_at', { ascending: true })
 		.select('*')
